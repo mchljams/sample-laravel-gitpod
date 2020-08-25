776af61171674e3a631420e49ecace1470eac288
mchljams <mike.pisula@gmail.com>
2020-08-25 04:07:24 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 composer create-project --prefer-dist laravel/laravel laravel
+cd ./laravel
 composer config repositories.repo-name vcs https://github.com/mchljams/travellog.git
 composer require mchljams/travellog:dev-master
